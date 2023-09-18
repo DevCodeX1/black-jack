5d6eefb4f5dad019095a54e4447fdab94a475610
DevCodeX <countryboya20@gmail.com>
2023-09-17 20:19:22 -0500

    added new card button

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 let firstCard = 10;
-let secondCard = 10;
-let sum = firstCard + secondCard
+let secondCard = 4;
+let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector(".sum")
+let cardsEl = document.getElementById("cards-el")
+
+
 
 function startGame(){
-if(sum <= 20){
-    message = ("Do you want to draw another card? 🃏")
-    
-}else if(sum === 21){
-    message =("Wohoo! You've got Black Jack 🎆")
-    hasBlackJack = true
-}else{
-    message =("Bust You Lose! ☠️")
-    isAlive = false
+    if(sum <= 20){
+        message = ("Do you want to draw a new card? 🃏")
+        
+    }else if(sum === 21){
+        message =("You've got Black Jack 🎆")
+        hasBlackJack = true
+    }else{
+        message =("Bust You Lose! ☠️")
+        isAlive = false
+    }
+    messageEl.textContent = message
+    sumEl.textContent = `Sum: ${sum}`
+    cardsEl.textContent = `Cards: ${firstCard}_${secondCard}`
 }
 
-console.log(hasBlackJack)
-console.log(isAlive)
-console.log(message)
+function newCard(){
+    console.log("new card")
+    let card = 7;
+    sum += card
+    startGame()
+
 }
