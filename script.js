217f96982a282e4db2700b888d1278df9e40d858
DevCodeX <countryboya20@gmail.com>
2023-09-19 08:43:22 -0500

    added alot of new functions

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard 
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.querySelector(".sum")
-let cardsEl = document.getElementById("cards-el")
+
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector(".sum");
+let cardsEl = document.getElementById("cards-el");
+let credits = document.getElementById("credits-el");
+
+let player = {
+    name: "David",
+    chips: 158
+}
+
+credits.textContent = `${player.name} $${player.chips}`
 
 
+function getRandomCard(){
+   let rCard = Math.floor(Math.random() * 13) + 1;
+   if (rCard > 10){
+    return 10;
+   }else if (rCard === 1){
+    return 11;
+   }else{
+    return rCard;
+   }
+   
+}
 
 function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard;
+    
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: " 
+    sumEl.textContent = `Sum: ${sum}`
+    
+    for(let i = 0; i < cards.length; i++){
+    cardsEl.textContent += ` ${cards[i]}`
+    }
+    
     if(sum <= 20){
         message = ("Do you want to draw a new card? 🃏")
-        
+       
     }else if(sum === 21){
         message =("You've got Black Jack 🎆")
         hasBlackJack = true
@@ -22,14 +58,15 @@ function startGame(){
         isAlive = false
     }
     messageEl.textContent = message
-    sumEl.textContent = `Sum: ${sum}`
-    cardsEl.textContent = `Cards: ${firstCard}_${secondCard}`
+    
 }
 
 function newCard(){
-    console.log("new card")
-    let card = 7;
+    if (isAlive === true && hasBlackJack === false){    
+    let card = getRandomCard();
     sum += card
-    startGame()
-
+    cards.push(card)
+    renderGame()
+    }
 }
+
